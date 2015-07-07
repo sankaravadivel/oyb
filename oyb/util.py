d78a4b36feebc8a46383f87f45d8d1904cfbb670
Sankaravadivel Sivarajan <sankara@elementsolutions.com>
2015-07-07 00:04:59 -0500

    added code to setup sqllite meta data repo

---
+++ oyb/util.py
@@ -0,0 +1,25 @@
+from os.path import expanduser
+
+USER_PREF_HOME_DIR_NAME = ".oyb"
+
+def get_user_pref_home(create_if_not_exists=True):
+    path = _get_user_pref_home()
+    if create_if_not_exists and not os.path.exists(path):
+        os.makedirs(path)
+    return path
+
+def _get_user_pref_home():
+    user_home = get_user_home()
+    pref_full_path = os.path.join(user_home, USER_PREF_HOME_DIR_NAME)
+    return pref_full_path
+
+def create_user_pref_home():
+    user_home = get_user_home()
+    pref_full_path = os.path.join(user_home, USER_PREF_HOME_DIR_NAME)
+    if not os.path.exists(pref_full_path):
+        os.makedirs(pref_full_path)
+
+def get_user_home():
+    return expanduser("~")
+
+                
